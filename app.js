5e81f9d922558b442efac2d01c38dbe6fc0b6d35
andycanderson <chen.a.andy@gmail.com>
2015-10-08 09:23:48 -0700

    allow crossorigin

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/stack', function (req, res) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:9000');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET');
   res.send(JSON.stringify({ a: 1 }));
 });
