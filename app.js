d594dcbe5203df05e5e163b177262ecfe0fe9efc
andycanderson <chen.a.andy@gmail.com>
2015-10-08 10:53:34 -0700

    update json

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var app = express();
 
-app.get('/stack', function (req, res) {
+app.get('/', function (req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET');
-  res.send(JSON.stringify({ a: 1 }));
+  res.send(JSON.stringify({[
+      {name: 'Riley'},
+      {name: 'Skip'},
+      {name: 'Minnie'},
+      {name: 'Penny'}
+  ]}));
 });
 
 var server = app.listen(process.env.PORT || 3000);
