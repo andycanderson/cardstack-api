c088f9a706892a7beb3e3910455f10cfb691e9aa
andycanderson <chen.a.andy@gmail.com>
2015-10-08 10:57:20 -0700

    update data

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ var app = express();
 app.get('/', function (req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET');
-  res.send(JSON.stringify({[
+  res.send(JSON.stringify([
       {name: 'Riley'},
       {name: 'Skip'},
       {name: 'Minnie'},
       {name: 'Penny'}
-  ]}));
+  ]));
 });
 
 var server = app.listen(process.env.PORT || 3000);
