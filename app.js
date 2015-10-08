f5f6af011bbdc42f80b505c312b11f37b9e7713e
andycanderson <chen.a.andy@gmail.com>
2015-10-08 09:37:17 -0700

    change port

--- app.js
+++ app.js
@@ -7,7 +7,4 @@ app.get('/stack', function (req, res) {
   res.send(JSON.stringify({ a: 1 }));
 });
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-});
+var server = app.listen(process.env.PORT || 3000);
