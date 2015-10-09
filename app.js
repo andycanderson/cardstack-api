2594104a9a1d39a72e439eec66afbf27ccfd272a
andycanderson <chen.a.andy@gmail.com>
2015-10-08 17:50:50 -0700

    add some images

--- app.js
+++ app.js
@@ -5,10 +5,22 @@ app.get('/', function (req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET');
   res.send(JSON.stringify([
-      {name: 'Riley'},
-      {name: 'Skip'},
-      {name: 'Minnie'},
-      {name: 'Penny'}
+      {
+        name: 'Riley',
+        image: 'http://i.imgur.com/hZ3AlAn.jpg',
+      },
+      {
+        name: 'Skip',
+        image: 'http://i.imgur.com/AwdYxQN.png',
+      },
+      {
+        name: 'Minnie',
+        image: 'http://i.imgur.com/tjvD7lA.jpg',
+      },
+      {
+        name: 'Penny',
+        image: 'http://i.imgur.com/cpsHZ22.jpg'
+      }
   ]));
 });
 
