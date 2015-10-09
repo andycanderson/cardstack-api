37bd4c363b6dfca37d2f768c631baa58f424947c
andycanderson <chen.a.andy@gmail.com>
2015-10-09 15:42:31 -0700

    update json

--- app.js
+++ app.js
@@ -7,19 +7,35 @@ app.get('/', function (req, res) {
   res.send(JSON.stringify([
       {
         name: 'Riley',
+        age: '25',
+        interests: 0,
+        friends: 1,
         image: 'http://i.imgur.com/hZ3AlAn.jpg',
+        actionable: true
       },
       {
         name: 'Skip',
+        age: '31',
+        interests: 0,
+        friends: 3,
         image: 'http://i.imgur.com/AwdYxQN.png',
+        actionable: true
       },
       {
-        name: 'Minnie',
+        name: 'Nala',
+        age: '27',
+        interests: 1,
+        friends: 2,
         image: 'http://i.imgur.com/tjvD7lA.jpg',
+        actionable: true
       },
       {
         name: 'Penny',
-        image: 'http://i.imgur.com/cpsHZ22.jpg'
+        age: '35',
+        interests: 0,
+        friends: 1,
+        image: 'http://i.imgur.com/cpsHZ22.jpg',
+        actionable: true
       }
   ]));
 });
